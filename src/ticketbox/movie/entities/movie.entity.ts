3159807001af7bc020f8c27e3011cd9efb4a697e
HaeChan Kim <bluesun147@naver.com>
2022-06-27 14:16:51 +0900

    [ADD] 선택지에 따른 영화 결과 출력

---
+++ src/ticketbox/movie/entities/movie.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Movie")
+export class Movie extends BaseEntity {
+    @PrimaryGeneratedColumn() 
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    image: string;
+}
