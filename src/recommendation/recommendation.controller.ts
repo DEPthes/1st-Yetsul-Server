89a8ddd645ba0a0aaf71a7be2e66ca6724d92007
HaeChan Kim <bluesun147@naver.com>
2022-07-12 14:31:24 +0900

    [Fix] 현재 날씨 정보 수정 (디폴트 서울)

--- src/recommendation/recommendation.controller.ts
+++ src/recommendation/recommendation.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { RecommendationService } from './recommendation.service';
 
@@ -6,11 +6,19 @@ import { RecommendationService } from './recommendation.service';
 export class RecommendationController {
   constructor(private readonly recommendationService: RecommendationService) { }
 
-  // 서울 날씨, 아이콘 조회
+  // 현재 날씨, 아이콘 조회
   @Get('/weather')
-  @ApiOperation({ summary: '서울 날씨, 아이콘 조회 API', description: '서울 날씨, 아이콘 조회' })
-  @ApiCreatedResponse({ description: '서울 날씨, 아이콘 조회' })
-  getWeather() {
-    return this.recommendationService.ofCity("seoul");
+  @ApiOperation({ summary: '현재 날씨, 아이콘 조회 API', description: '현재 날씨, 아이콘 조회' })
+  @ApiCreatedResponse({ description: '현재 날씨 조회. 사용자가 위치 정보 허용 하면 사용자의 위치로, 안하면 위치 서울로.' })
+  getTodaysWeather(@Body('lat') lat: string, @Body('lon') lon: string) {
+    return this.recommendationService.getTodaysWeather(lat, lon);
+  }
+
+  // 날씨에 따른 술 추천
+  @Get('/weather/:id')
+  @ApiOperation({ summary: '날씨에 따른 술 추천 API', description: '날씨에 따른 술 추천' })
+  @ApiCreatedResponse({ description: '날씨에 따른 술 추천' })
+  getWeatherRecommendation(@Param('id') id: string) {
+    return this.recommendationService.getWeatherRecommendation(id);
   }
 }
