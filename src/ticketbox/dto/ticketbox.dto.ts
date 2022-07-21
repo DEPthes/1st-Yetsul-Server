430925b56a8f98408edff5597952d97cce4073f0
hyunbin1 <kimgusqls1@gmail.com>
2022-07-21 14:56:14 +0900

    [Delete] 필요없는 dto 제거

--- src/ticketbox/dto/ticketbox.dto.ts
+++
@@ -1,8 +0,0 @@
-import {IsString } from "class-validator";
-
-export class ticketboxDto {
-    @IsString() 
-    readonly questionContent: string;
-    @IsString() 
-    readonly selectionContent: string;
-}
