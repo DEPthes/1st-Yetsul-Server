468e83cf121d7557efa43da9369d99b6e202c0cb
HaeChan Kim <bluesun147@naver.com>
2022-11-16 14:27:56 +0900

    close #36 [REFACTOR] .env 제거

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,23 +3,11 @@ import 'dotenv/config';
 
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST, // 'depth-db.cz96nnlicngq.ap-northeast-2.rds.amazonaws.com',
-    port: 5432,
-    username: process.env.DB_USERNAME, // 'postgres',
-    password: process.env.DB_PASSWORD, // 'depth2022',
-    // database:  process.env.DB_DATABASE, // 'postgres',
-    database:  'post2', // 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database:  process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js, ts}'], // entities: [__dirname + '/../**/*.entity.{js, ts}', __dirname + '/../**/*.repository.{js, ts}'],
     synchronize: true,
 }
-
-// export const typeORMConfig : TypeOrmModuleOptions = {
-//     type: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     username: 'postgres',
-//     password: 'bluesun', // DepthLocalTest, 맨 처음 로그인 한 비밀번호랑 같아야 함.
-//     database: 'postgres',
-//     entities: [__dirname + '/../**/*.entity.{js, ts}'], // entities: [__dirname + '/../**/*.entity.{js, ts}', __dirname + '/../**/*.repository.{js, ts}'],
-//     synchronize: true,
-// }
